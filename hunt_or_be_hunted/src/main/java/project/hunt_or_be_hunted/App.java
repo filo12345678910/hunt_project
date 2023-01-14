28b9e6ac37900675d02d5086959242153bd195af
filo12345678910 <filip.firkowski@gmail.com>
2023-01-14 01:23:45 +0100

    ACTUALL GUI START
    change from canvas to grid, add pngs

--- hunt_or_be_hunted/src/main/java/project/hunt_or_be_hunted/App.java
+++ hunt_or_be_hunted/src/main/java/project/hunt_or_be_hunted/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         
-        scene = new Scene(loadFXML("GUI"), 640, 480);
+        scene = new Scene(loadFXML("GUI"), 1440, 810);
         stage.setScene(scene);
         stage.show();
     }
--- hunt_or_be_hunted/src/main/java/project/hunt_or_be_hunted/GUIController.java
+++ hunt_or_be_hunted/src/main/java/project/hunt_or_be_hunted/GUIController.java
@@ -6,13 +6,9 @@ package project.hunt_or_be_hunted;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 
 /**
  * FXML Controller class
@@ -21,8 +17,6 @@ import javafx.scene.paint.Color;
  */
 public class GUIController implements Initializable {
 
-    @FXML
-    private Canvas board;
 
     /**
      * Initializes the controller class.
@@ -30,20 +24,9 @@ public class GUIController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-
-    @FXML
-    private void stuff(ActionEvent event) {
-        System.out.println("aaa");
     }
 
     @FXML
-    private void stuff1(MouseEvent event) {
-        System.out.println("bbb");
-    }
-    public void draw_board(){
-        GraphicsContext gc = board.getGraphicsContext2D();
-        gc.setFill(Color.BLUE);
-        gc.fillRect(75,75,100,100);
+    private void getInfo(MouseEvent event) {
     }
 }
