40a0df96e83f660a8c4f414896efb040d13c5a76
filo12345678910 <filip.firkowski@gmail.com>
2023-01-25 06:41:12 +0100

    Revert "Revert "start predator""
    This reverts commit 4d5a037aa4ee6e0d677c91912f239693056588d6.

--- hunt_or_be_hunted/src/main/java/project/hunt_or_be_hunted/Prey.java
+++ hunt_or_be_hunted/src/main/java/project/hunt_or_be_hunted/Prey.java
@@ -131,7 +131,6 @@ public class Prey extends Animal implements Runnable {
     public void run() {
         int goalX = -1;
         int goalY = -1;
-        System.out.println(info());
         while(isIsAlive() == true){
         if ((goalX == -1 || goalY == -1) || (goalX == x && goalY == y)){
             if (water_level < 50){
@@ -149,7 +148,6 @@ public class Prey extends Animal implements Runnable {
                 goalX = goal.getX();
                 goalY= goal.getY();
             }
-            System.out.println("GOAL:" + goalX + " " + goalY);
         }
         if ((water_level < 100) && (GUIController.getEnviromentByCoordinate(getX(), getY()) instanceof Water_source)){
             try {
